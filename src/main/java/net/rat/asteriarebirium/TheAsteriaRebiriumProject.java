e280de1bb621f2eaeef2e4da691f6d91eeb2da37
St4rRat <chaosman227@gmail.com>
2024-08-31 18:03:47 -0400

    Initial commit

---
+++ src/main/java/net/rat/asteriarebirium/TheAsteriaRebiriumProject.java
@@ -0,0 +1,16 @@
+package net.rat.asteriarebirium;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheAsteriaRebiriumProject implements ModInitializer {
+	public static final String MOD_ID = "asteriarebirium";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/rat/asteriarebirium/TheAsteriaRebiriumProjectClient.java
@@ -0,0 +1,10 @@
+package net.rat.asteriarebirium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheAsteriaRebiriumProjectClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
